dd020596af21bbe34f8c1ffac6f47affdb165943
JustinChang <c24971720@gmail.com>
2025-07-02 22:03:22 +0800

    add list, dict api

--- libtest.pyx
+++ libtest.pyx
@@ -1,6 +1,8 @@
+# cython: language_level=3
 # distutils: language = c
-
 from cpython.ref cimport PyObject
+from cpython.list cimport PyList_Check, PyList_Size
+from cpython.dict cimport PyDict_Check
 from libc.stdlib cimport malloc, free
 import sys
 
@@ -14,3 +16,25 @@ def get_foo():
 
 def get_bar():
     return bar.bar()
+
+def process_list(py_list):
+    """
+    Accepts a Python list and returns its length.
+    """
+    if not PyList_Check(py_list):
+        raise TypeError("Expected a Python list")
+    return PyList_Size(py_list)
+
+def process_dict(py_dict):
+    """
+    Accepts a Python dict and returns the list of keys.
+    """
+    if not PyDict_Check(py_dict):
+        raise TypeError("Expected a Python dict")
+    return list(py_dict.keys())
+
+def process_class(obj):
+    """
+    Accepts a Python class instance and returns its class name.
+    """
+    return obj.__class__.__name__
--- main.c
+++ main.c
@@ -48,6 +48,90 @@ int main(int argc, char *argv[]) {
         PyErr_Print();
     }
 
+    // Call process_list([1, 2, 3])
+    pFunc = PyObject_GetAttrString(pModule, "process_list");
+    if (pFunc && PyCallable_Check(pFunc)) {
+        PyObject *pList = PyList_New(3);
+        PyList_SetItem(pList, 0, PyLong_FromLong(1));
+        PyList_SetItem(pList, 1, PyLong_FromLong(2));
+        PyList_SetItem(pList, 2, PyLong_FromLong(3));
+        PyObject *pArgs = PyTuple_Pack(1, pList);
+        pValue = PyObject_CallObject(pFunc, pArgs);
+        if (pValue) {
+            printf("process_list([1,2,3]) returned: %ld\n", PyLong_AsLong(pValue));
+            Py_DECREF(pValue);
+        } else {
+            PyErr_Print();
+        }
+        Py_DECREF(pArgs);
+        Py_DECREF(pList);
+        Py_DECREF(pFunc);
+    } else {
+        PyErr_Print();
+    }
+
+    // Call process_dict({'a': 1, 'b': 2})
+    pFunc = PyObject_GetAttrString(pModule, "process_dict");
+    if (pFunc && PyCallable_Check(pFunc)) {
+        PyObject *pDict = PyDict_New();
+        PyDict_SetItemString(pDict, "a", PyLong_FromLong(1));
+        PyDict_SetItemString(pDict, "b", PyLong_FromLong(2));
+        PyObject *pArgs = PyTuple_Pack(1, pDict);
+        pValue = PyObject_CallObject(pFunc, pArgs);
+        if (pValue) {
+            printf("process_dict({'a':1,'b':2}) returned keys: ");
+            Py_ssize_t len = PyList_Size(pValue);
+            for (Py_ssize_t i = 0; i < len; ++i) {
+                PyObject *item = PyList_GetItem(pValue, i);
+                printf("%s ", PyUnicode_AsUTF8(item));
+            }
+            printf("\n");
+            Py_DECREF(pValue);
+        } else {
+            PyErr_Print();
+        }
+        Py_DECREF(pArgs);
+        Py_DECREF(pDict);
+        Py_DECREF(pFunc);
+    } else {
+        PyErr_Print();
+    }
+
+    // Call process_class(foo.foo())
+    pFunc = PyObject_GetAttrString(pModule, "process_class");
+    if (pFunc && PyCallable_Check(pFunc)) {
+        PyObject *fooModule = PyImport_ImportModule("foo");
+        if (fooModule) {
+            PyObject *fooFunc = PyObject_GetAttrString(fooModule, "foo");
+            if (fooFunc && PyCallable_Check(fooFunc)) {
+                PyObject *fooObj = PyObject_CallObject(fooFunc, NULL);
+                if (fooObj) {
+                    PyObject *pArgs = PyTuple_Pack(1, fooObj);
+                    pValue = PyObject_CallObject(pFunc, pArgs);
+                    if (pValue) {
+                        printf("process_class(foo.foo()) returned: %s\n", PyUnicode_AsUTF8(pValue));
+                        Py_DECREF(pValue);
+                    } else {
+                        PyErr_Print();
+                    }
+                    Py_DECREF(pArgs);
+                    Py_DECREF(fooObj);
+                } else {
+                    PyErr_Print();
+                }
+                Py_DECREF(fooFunc);
+            } else {
+                PyErr_Print();
+            }
+            Py_DECREF(fooModule);
+        } else {
+            PyErr_Print();
+        }
+        Py_DECREF(pFunc);
+    } else {
+        PyErr_Print();
+    }
+
     Py_DECREF(pModule);
 
     Py_Finalize();
